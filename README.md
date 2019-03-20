d4bf4b0bc8f854af2d9be05b3f62b8765a08afad
Paul Hulbert <pxh8242@rit.edu>
2019-03-20 11:15:10 -0400

    Testing full process

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # NewsProject
-SWEN-344 News Feed Project1
+SWEN-344 News Feed Project
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! Test"
+    return "Hello World! Test2"
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
